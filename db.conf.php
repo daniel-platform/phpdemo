497fa60833d91e7f3089bc75994d1161099d9a24
Daniel Morante <daniel@platform.sh>
2015-12-17 14:15:48 -0500

    Add missing lines

--- db.conf.php
+++ db.conf.php
@@ -1,6 +1,8 @@
 <?php
-define("DB_HOST", $relationships[$database_name][0]['host']);
-define("DB_USER", $relationships[$database_name][0]['username']);
-define("DB_PASS", $relationships[$database_name][0]['password']);
-define("DB_NAME", $relationships[$database_name][0]['path']);
+$relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), TRUE);
+
+define("DB_HOST", $relationships['database'][0]['host']);
+define("DB_USER", $relationships['database'][0]['username']);
+define("DB_PASS", $relationships['database'][0]['password']);
+define("DB_NAME", $relationships['database'][0]['path']);
 ?>
